ce852e4bc9196f9f16142d56adbbdfeebd303682
Nicolas Herbaut <nicolas.herbaut@gmail.com>
2022-05-31 16:37:41 +0200

    fixes #3 you can shake the tree

--- src/test/java/fr/pantheonsorbonne/cri/TreeTest.java
+++ src/test/java/fr/pantheonsorbonne/cri/TreeTest.java
@@ -3,6 +3,9 @@ package fr.pantheonsorbonne.cri;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class TreeTest {
 
 
@@ -15,6 +18,20 @@ class TreeTest {
         tree.addApple(a2);
         Assertions.assertTrue(tree.hasApples());
 
+    }
+
+    @Test
+    void treeShake() {
+        Tree t = new Tree();
+        for (int i = 0; i < 100; i++) {
+            t.addApple(new Apple());
+        }
+        Set<Apple> collector = new HashSet<>();
+        while (t.hasApples()) {
+            collector.addAll(t.shakeTree());
+        }
+
+        Assertions.assertEquals(100, collector.size());
     }
 
     @Test
